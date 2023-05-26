3ec9153f1812f94dab2aad3c329f628ffd954379
Mark Joshua Dichoso <129643517+JoshuaDichoso@users.noreply.github.com>
2023-05-26 20:17:41 +0800

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,101 +1,120 @@
 using System;
+using System.Collections.Generic;
 
 class JewelryOrderingSystem
 {
+    static List<string> necklaces = new List<string>()
+    {
+        
+        "Gold Chain Necklace",
+        "Pearl Necklace",
+        "Silver Necklace",
+        "Iron Chain",
+        "Diamond Necklace",
+        "Yellow Gold Necklace",
+        "Quartz Necklace"
+    };
+
+    static Dictionary<string, decimal> necklacePrices = new Dictionary<string, decimal>()
+    {
+        
+        { "Gold Chain Necklace", 1000m },
+        { "Pearl Necklace", 400m },
+        { "Silver Necklace", 600m },
+        { "Iron Chain", 100m },
+        { "Diamond Necklace", 5000m },
+        { "Yellow Gold Necklace", 9000m },
+        { "Quartz Necklace", 50m }
+    };
+
+    static Dictionary<string, List<string>> customizations = new Dictionary<string, List<string>>()
+    {
+        
+        { "Gold Chain Necklace", new List<string>() { "Locket", "Princess Necklace", "Choker", "Multi Chain", "Negligee" } },
+        { "Pearl Necklace", new List<string>() { "Locket", "Princess Necklace", "Choker", "Multi Chain", "Negligee" } },
+        { "Silver Necklace", new List<string>() { "Locket", "Princess Necklace", "Choker", "Multi Chain", "Negligee" } },
+        { "Iron Chain", new List<string>() { "Locket", "Princess Necklace", "Choker", "Multi Chain", "Negligee" } },
+        { "Diamond Necklace", new List<string>() { "Locket", "Princess Necklace", "Choker", "Multi Chain", "Negligee" } },
+        { "Yellow Gold Necklace", new List<string>() { "Locket", "Princess Necklace", "Choker", "Multi Chain", "Negligee" } },
+        { "Quartz Necklace", new List<string>() { "Locket", "Princess Necklace", "Choker", "Multi Chain", "Negligee" } }
+    };
+
     static void Main()
     {
-        const decimal GoldChainPrice = 1000m;
-        const decimal PearlNecklacePrice = 400m;
-        const decimal SilverNecklacePrice = 600m;
-        const decimal IronChainPrice = 100m;
-        const decimal DiamondNecklacePrice = 5000m;
-        const decimal YellowGoldNecklacePrice = 9000m;
-        const decimal QuartzNecklacePrice = 50m;
-
-
         Console.WriteLine("Welcome to the MJ's Necklace Jewelry Ordering System!");
-        
-        Console.WriteLine("Please select the type of necklace you want to order:");
-        Console.WriteLine("1. Gold Chain Necklace ($1000)");
-        Console.WriteLine("2. Pearl Necklace ($400)");
-        Console.WriteLine("3. Silver Necklace ($600)");
-        Console.WriteLine("4. Iron Chain ($100)");
-        Console.WriteLine("5. Diamond Necklace ($5000");
-        Console.WriteLine("6. Yellow Gold Necklace ($9000)");
-        Console.WriteLine("7. Quartz Necklace ($400)");
+
+        DisplayNecklaces();
 
         Console.Write("Enter your jewelry of choice: ");
-        string choice = Console.ReadLine();
+        int choice = int.Parse(Console.ReadLine());
 
-        decimal totalPrice = 0m;
-        string necklaceType = "";
-
-        switch (choice)
+        if (choice < 1 || choice > necklaces.Count)
         {
-            
-            case "1":
-                totalPrice = GoldChainPrice;
-                necklaceType = "Gold Chain Necklace";
-                break;
-            case "2":
-                totalPrice = PearlNecklacePrice;
-                necklaceType = "Pearl Necklace";
-                break;
-            case "3":
-                totalPrice = SilverNecklacePrice;
-                necklaceType = "Silver Necklace";
-                break;
-            case "4":
-                totalPrice = IronChainPrice;
-                necklaceType = "Iron Chain";
-                break;
-            case "5":
-                totalPrice = DiamondNecklacePrice;
-                necklaceType = "Diamond Necklace";
-                break;
-            case "6":
-                totalPrice = YellowGoldNecklacePrice;
-                necklaceType = "Yellow Gold Necklace";
-                break;
-            case "7":
-                totalPrice = QuartzNecklacePrice;
-                necklaceType = "Quartz Necklace";
-                break;
-
-            default:
-                Console.WriteLine("Invalid choice!");
-                return;
+            Console.WriteLine("Invalid choice!");
+            return;
         }
-        
-        Console.WriteLine("You have selected: " + necklaceType);
-        Console.WriteLine("Total Price: $" + totalPrice);
+
+        string selectedNecklace = necklaces[choice - 1];
+        List<string> customizationOptions = customizations[selectedNecklace];
+
+        Console.WriteLine("You have selected: " + selectedNecklace);
+        Console.WriteLine("Please select the customization option:");
+
+        for (int i = 0; i < customizationOptions.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {customizationOptions[i]}");
+        }
+
+        Console.Write("Enter your customization choice: ");
+        int customizationChoice = int.Parse(Console.ReadLine());
+
+        if (customizationChoice < 1 || customizationChoice > customizationOptions.Count)
+        {
+            Console.WriteLine("Invalid customization choice!");
+            return;
+        }
+
+        string selectedCustomization = customizationOptions[customizationChoice - 1];
+        string necklaceType = $"{selectedNecklace} ({selectedCustomization})";
+
+        decimal necklacePrice = necklacePrices[selectedNecklace];
+        Console.WriteLine("Total Price: $" + necklacePrice);
 
         Console.Write("Enter your name: ");
         string customerName = Console.ReadLine();
-
         Console.WriteLine("Thank you, " + customerName + "!");
 
         Console.Write("Enter your payment amount: $");
         decimal paymentAmount = decimal.Parse(Console.ReadLine());
 
-        if (paymentAmount < totalPrice)
+        if (paymentAmount < necklacePrice)
         {
             Console.WriteLine("Insufficient payment!");
             return;
         }
 
-        decimal change = paymentAmount - totalPrice;
+        decimal change = paymentAmount - necklacePrice;
         Console.WriteLine("Payment successful. Your change: $" + change);
+        
 
         Console.WriteLine();
         Console.WriteLine("Order Processing and Receipt");
         Console.WriteLine("----------------------------");
         Console.WriteLine("Customer Name: " + customerName);
         Console.WriteLine("Necklace Type: " + necklaceType);
-        Console.WriteLine("Total Price: $" + totalPrice);
+        Console.WriteLine("Total Price: $" + necklacePrice);
         Console.WriteLine("Payment Amount: $" + paymentAmount);
         Console.WriteLine("Change: $" + change);
         Console.WriteLine("Thank you for your order!");
+    }
 
+    static void DisplayNecklaces()
+    {
+        Console.WriteLine("Please select the type of necklace you want to order:");
+
+        for (int i = 0; i < necklaces.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {necklaces[i]}");
+        }
     }
 }
